501b7dc2b0d9cab3d0bd5c557b4f57bbb9ade998
ManolisPapa <150335568+ManolisPapa@users.noreply.github.com>
2023-12-04 21:29:54 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner readAns = new Scanner(System.in);
+        int ans;
+        while (true) {
+            System.Out.Println("welcome to chat aueb. if u wish to sign up press 1. If u are already registered press 2")
+            readAns = read.nextInt();
+            if (ans == 1) {
+                break;
+            } else if (ans == 2) {
+                break;
+            }
+        }
+        readAns.close();
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Please insert your username");
+        String username = read.nextLine();
+        System.out.println("Please insert your password");
+        String password = read.nextLine();
+
+    }
+}
