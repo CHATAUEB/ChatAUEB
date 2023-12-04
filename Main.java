df24d5988d042ae4fb200a3727004e1149f63cf1
ManolisPapa <150335568+ManolisPapa@users.noreply.github.com>
2023-12-04 21:29:30 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String message = "Katalbaineis na mou apantiseis greeklish toulaxiston?";
-        System.out.println(Message.chatGPT(message));
-    }
-}
