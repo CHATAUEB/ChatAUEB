8bb429f5dfbd83724bcd6eafc9e3073df25b94df
Niggos <150166515+niksin21@users.noreply.github.com>
2023-12-05 13:41:23 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,18 +7,20 @@ public class Main {
             System.Out.Println("welcome to chat aueb. if u wish to sign up press 1. If u are already registered press 2")
             readAns = read.nextInt();
             if (ans == 1) {
+                User(String username, String password);
+                System.out.println("Please insert your username");
+                String username = read.nextLine();
+                System.out.println("Please insert your password");
+                String password = read.nextLine();
                 break;
             } else if (ans == 2) {
+                //connect(log in)
                 break;
             }
         }
         readAns.close();
         Scanner read = new Scanner(System.in);
 
-        System.out.println("Please insert your username");
-        String username = read.nextLine();
-        System.out.println("Please insert your password");
-        String password = read.nextLine();
-
+      
     }
 }
