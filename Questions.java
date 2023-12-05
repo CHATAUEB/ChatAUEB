1ebea6fc7107f6e11b0e2f7c57d89946bc456eed
ManolisPapa <150335568+ManolisPapa@users.noreply.github.com>
2023-12-05 16:58:46 +0200

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -1,8 +1,11 @@
 public class Questions {
-    public static final String[] questions;
+    
+    public static final int size = 30;
+    //The questionnaire consists of 30 questions
+    public static final String[] questions = new String[size];
     //TODO find a way to add the questions into the array
-
-    public static final String[] type;
+    
+    public static final String[] type = new String[size];
     //Saves the type of answer expected from the user, e.g. text, numerical etc.
 
 }
