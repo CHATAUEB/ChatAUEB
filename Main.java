06659f14501b38c2a280243de976120df565aaf5
SpiliosDmkk <150211937+SpiliosDmkk@users.noreply.github.com>
2023-12-05 14:04:10 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -30,3 +30,84 @@ public class Main {
       
     }
 }
+import java.util.Scanner;
+
+public class UserMenu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Welcome to the User Menu!");
+            System.out.println("1. Log In");
+            System.out.println("2. Sign Up");
+            System.out.println("3. Change Password");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    loginUser();
+                    break;
+                case 2:
+                    signUpUser();
+                    break;
+                case 3:
+                    changePassword();
+                    break;
+                case 4:
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option. Please choose a valid option.");
+            }
+        }
+    }
+
+    private static void loginUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        User user = User.connect(username, password);
+
+        if (user != User.nullUser) {
+            System.out.println("Login successful! Welcome, " + user.username);
+            // You can add more options or actions for a logged-in user here
+        } else {
+            System.out.println("Login failed. Invalid username or password.");
+        }
+    }
+
+    private static void signUpUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        new User(username, password);
+        // You might want to add more actions after successful signup
+    }
+
+    private static void changePassword() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter current password: ");
+        String password = scanner.nextLine();
+
+        User user = User.connect(username, password);
+
+        if (user != User.nullUser) {
+            System.out.print("Enter new password: ");
+            String newPassword = scanner.nextLine();
+            user.SetPassword(newPassword);
+        } else {
+            System.out.println("Change password failed. Invalid username or password.");
+        }
+    }
+}
