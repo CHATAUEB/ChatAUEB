2fb3d11c21817308ba7a621d403acbf3548e2624
ManolisPapa <150335568+ManolisPapa@users.noreply.github.com>
2023-12-05 16:57:29 +0200

    Update Answer.java

--- Answer.java
+++ Answer.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public abstract class Answer {
+
     public String answer;
     public Type type; 
-
     public abstract Answer(); //Default constructor, must be overriden by subclasses
     
     public static Answer readAnswer(int i) {
         System.out.println(Questions.questions[i]);
         Scanner read = new Scanner(System.in);
         String temp = read.nextLine();
-        checkAnswer(temp);
+        boolean flag = checkAnswer(temp);
     }
 
     public abstract static boolean checkAnswer(int i);
