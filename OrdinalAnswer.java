978bbba4ddd6e72f3df8dea1ff3abbcacc368465
ManolisPapa <150335568+ManolisPapa@users.noreply.github.com>
2023-12-05 16:58:27 +0200

    Update OrdinalAnswer.java

--- OrdinalAnswer.java
+++ OrdinalAnswer.java
@@ -5,5 +5,10 @@ public class OrdinalAnswer extends Answer {
         this.answer = answer.toString();
         this.type = Ordinal;
     }
+
+    @Override
+    public static boolean checkAnswer(int i) {
+        //TODO evaluation of Ordinal values.This needs to be done by the application not the AI
+    }
     
 }
