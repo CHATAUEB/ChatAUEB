4c61c17ee4d7a7bfa5747532b84199520454a739
SpiliosDmkk <150211937+SpiliosDmkk@users.noreply.github.com>
2023-12-12 14:26:09 +0200

    Add files via upload

---
+++ Gui.java
@@ -0,0 +1,84 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Gui {
+
+    private static JFrame frame;
+
+    public static void openFrame() {
+        frame = new JFrame("CHATAUEB - Menu");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(500, 500);
+
+        TextFrame();
+        createButtons(); // Call createButtons to add buttons to the frame
+
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    public static void TextFrame() {
+        JLabel label = new JLabel("Welcome to ChatAUEB.");
+        label.setBounds(10, 10, 480, 100); // Adjusted the width to 480 for centering
+        label.setHorizontalAlignment(JLabel.CENTER); // Center the text
+
+        // Set the font to bold
+        Font boldFont = new Font(label.getFont().getFontName(), Font.BOLD, 20);
+        label.setFont(boldFont);
+
+        frame.add(label);
+    }
+
+    public static void createButtons() {
+        JLabel label = new JLabel("Welcome to ChatAUEB.");
+        int labelHeight = 100; // Height of the label
+
+        // Add buttons for sign up, log in, and change password
+        JButton signUpButton = new JButton("Sign");
+        signUpButton.setBounds(10, labelHeight + 20, 100, 30); // Centered vertically based on label height
+        frame.add(signUpButton);
+
+        JButton loginButton = new JButton("Log In");
+        loginButton.setBounds(10, labelHeight + 60, 100, 30); // Centered vertically based on label height
+        frame.add(loginButton);
+
+        JButton changePasswordButton = new JButton("Change Password");
+        changePasswordButton.setBounds(10, labelHeight + 100, 150, 30); // Centered vertically based on label height
+        frame.add(changePasswordButton);
+
+        // Add action listeners for the buttons
+        signUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Sign Up button pressed");
+                // Add your sign-up logic here
+            }
+        });
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Log In button pressed");
+                // Add your log-in logic here
+            }
+        });
+
+        changePasswordButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Change Password button pressed");
+                // Add your change password logic here
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        openFrame();
+    }
+}
