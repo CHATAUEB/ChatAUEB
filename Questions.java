607ec1502fc635675960ba02b1b2ef35c537d3df
ManolisPapa <150335568+ManolisPapa@users.noreply.github.com>
2023-12-21 21:41:06 +0200

    Add files via upload

--- Questions.java
+++ Questions.java
@@ -1,4 +1,15 @@
 public class Questions {
-    public static final String[][] questions = new String[User.answersLength][6];
-    //TODO find a way to add the questions into the array4
+    public static final String[][] fullQuestions = new String[User.answersLength][7];
+
+    public static void createQuestions() {
+        fullQuestions[0][0] = "Σε ποια μαθήματα εχετε επιτύχει περισσότερο ή εχετε εκδηλώσει ιδιαίτερο ενδιαφέρον στο λυκειο;";
+        fullQuestions[0][1] = "Μαθηματικά";
+        fullQuestions[0][2] = "Οικονομικά";
+        fullQuestions[0][3] = "Διοίκηση Επιχειρήσεων";
+        fullQuestions[0][4] = "Πληροφορικη";
+        fullQuestions[0][5] = "Σε όλα";
+        fullQuestions[0][6] = "Σε κανένα από αυτά";
+        fullQuestions[1][0] = "Σε τι τομεις θα θελατε να εργαστειτε μελλοντικα;";
+        fullQuestions[1][1] = "Μάρκετινγκ και Επικοινωνίες";
+    }
 }
