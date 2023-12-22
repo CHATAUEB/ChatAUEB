f4d79a96c02e94a7f547dd3dd9015d81c0e5785e
ManolisPapa <150335568+ManolisPapa@users.noreply.github.com>
2023-12-23 00:52:33 +0200

    Add files via upload

--- QueryBuilder.java
+++ QueryBuilder.java
@@ -14,10 +14,10 @@ public class QueryBuilder {
                         "Ο μαθητης απαντησε στο παρακατω ερωτηματολογιο οσων αφορα στις προτιμησεις του: \r\n";
 
     //Define a default question for Chatgpt: choose a department based on answers
-    final String defaultQuestion = "Ποιο τμήμα να διαλέξει ο χρήστης;"
+    final static String defaultQuestion = "Ποιο τμήμα να διαλέξει ο χρήστης;";
 
     // Constructor method that builds the query
-    public String QueryBuilder(questions[], answer[], message) {
+    public static String query(String[] questions,String[] answer,String message) {
         
         // Initialize a StringBuilder to construct the final message
         StringBuilder builder = new StringBuilder();
@@ -30,17 +30,30 @@ public class QueryBuilder {
             builder.append(questions[i] + ": ");
             builder.append(answer[i] + "\n");
         }
-
-        // Append the default question
-        builder.append(defaultQuestion);
         
         // Append an additional message if provided
-        if (message != null) {
+        if (message != "") {
             builder.append(message);
+        } else {
+            builder.append(defaultQuestion + "\n");
         }
         
         // Convert the StringBuilder to a String and return it
         String toChat = builder.toString();
         return toChat;
     }
+
+    public static void main(String args[]) {
+        String[] questions = new String[3];
+        String[] answers = new String[3];
+        String message = "";
+        questions[0] = "One";
+        questions[1] = "Two";
+        questions[2] = "Three";
+        answers[0] = "1";
+        answers[1] = "2";
+        answers[2] = "3";
+        System.out.println(QueryBuilder.query(questions, answers, message));
+    }
 }
+
