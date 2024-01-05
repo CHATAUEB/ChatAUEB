1001de0ed413e5af5deb8d54f33c68bb07a5bd8c
SpiliosDmkk <150211937+SpiliosDmkk@users.noreply.github.com>
2024-01-05 15:32:32 +0200

    Update UnitTest.java

--- UnitTest.java
+++ UnitTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class UnitTest {
   
-  //QueryBuilder class
+  //QueryBuilder class Unit Test
    @Test
     public void testQueryGeneration() {
         String result = QueryBuilder.query(questions, answers, message);
@@ -14,6 +14,26 @@ public class UnitTest {
                 result.contains(QueryBuilder.TEXT));
         Assert.assertTrue("Generated query does not contain expected questions",
                 result.contains(QueryBuilder.defaultQuestion));
+    }
+  
+    @Test
+    public void testMismatchedLengths() {
+        // Mismatched lengths of questions and answers arrays
+        String[] mismatchedQuestions = {"One", "Two", "Three"};
+        String[] mismatchedAnswers = {"1", "2"};
+        QueryBuilder.query(mismatchedQuestions, mismatchedAnswers, message);
+    }
+
+    @Test
+    public void testNullQuestions() {
+        questions = null;
+        QueryBuilder.query(questions, answers, message);
+    }
+
+    @Test
+    public void testNullAnswers() {
+        answers = null;
+        QueryBuilder.query(questions, answers, message);
     }
 
 }
