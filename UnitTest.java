5bd966e015aca1a8a969f2154753a2a47c6e4b7d
SpiliosDmkk <150211937+SpiliosDmkk@users.noreply.github.com>
2024-01-05 15:27:04 +0200

    Update UnitTest.java

--- UnitTest.java
+++ UnitTest.java
@@ -1,3 +1,19 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class UnitTest {
+  
+  //QueryBuilder class
+   @Test
+    public void testQueryGeneration() {
+        String result = QueryBuilder.query(questions, answers, message);
+        Assert.assertNotNull("Generated query is null", result);
+        Assert.assertTrue("Generated query does not contain expected information",
+                result.contains(QueryBuilder.TEXT));
+        Assert.assertTrue("Generated query does not contain expected questions",
+                result.contains(QueryBuilder.defaultQuestion));
+    }
 
 }
