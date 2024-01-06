31dcbf409f6ea8ba2bb3948c421b0b4ad56cffa5
ManolisPapa <150335568+ManolisPapa@users.noreply.github.com>
2024-01-06 22:56:33 +0200

    Update Error.java

--- Error.java
+++ Error.java
@@ -1,15 +1,34 @@
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+/**
+*A class used to show different error messages during the registration process
+*/
+
 public class Error {
     private String errorName;
     private String errorMessage;
 
+    /**
+    *Error constructor used to create static objects for later reference
+    *@param errorName the name of an Error object
+    *@param errorMessage the message the Error object displays
+    */
+    
     public Error(String errorName, String errorMessage) {
         this.errorName = errorName;
         this.errorMessage = errorMessage;
     }
 
+    /**
+    *Displays a message signifiying that an error has occured during the registration process
+    *<p>
+    *This method utilizes JOptionPane in order to display the error message
+    *@param error the Error object related to the error that has occured
+    *@param frame the JFrame which called this method
+    *@see JOptionPane
+    */
+    
     public static void displayError(Error error, JFrame frame) {
         JOptionPane.showMessageDialog(frame, error.errorMessage, error.errorName, JOptionPane.ERROR_MESSAGE);
     }
